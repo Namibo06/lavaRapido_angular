45d7f20307c7e445d63f99f5330ee41ba4dd2839
Namibo06 <namibo654@gmail.com>
2024-02-29 17:04:55 -0300

    feat: create componentes pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header/header.component';
 import { FooterComponent } from './shared/footer/footer/footer.component';
+import { RegisterComponent } from './pages/register/register/register.component';
+import { LoginComponent } from './pages/login/login/login.component';
+import { IndexComponent } from './pages/index/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
