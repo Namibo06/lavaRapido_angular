50e6223908dc3761b8a7f7fb34306585b2a030ac
Namibo06 <namibo654@gmail.com>
2024-02-29 19:21:06 -0300

    build:register

--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [RegisterComponent],
+  exports:[RegisterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class RegisterModule { }
--- src/app/pages/register/register/register.component.ts
+++ src/app/pages/register/register/register.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  first_name:string='';
+  last_name:string='';
+  email:string='';
+  phone:string='';
+  password:string='';
+  confirm_password:string='';
 
+  token:string='';
+  status:number=0;
+  message:string='';
+  callback:boolean=false;
+  messageError:string='';
+  callbackError:boolean=false;
+
+  constructor(){
+
+  }
+
+  registrar(){
+    
+  }
 }
