110ef9a8389b55f421a27da19ba5de5b5f77b057
Namibo06 <namibo654@gmail.com>
2024-02-29 19:16:36 -0300

    style: header & footer

--- src/app/shared/footer/footer/footer.component.ts
+++ src/app/shared/footer/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer-shared',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css','./footer.responsive.component.css']
 })
 export class FooterComponent {
   date:number=new Date().getFullYear();
--- src/app/shared/header/header/header.component.ts
+++ src/app/shared/header/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header-shared',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css','./header.responsive.component.css']
 })
 export class HeaderComponent {
 
