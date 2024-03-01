17dc19f3d9982672e2060fcac06366bb5af33962
Namibo06 <namibo654@gmail.com>
2024-03-01 16:57:31 -0300

    build:api-car-wash-service

--- src/app/services/api-car-wash.service.ts
+++ src/app/services/api-car-wash.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiCarWashService {
+  urlRetornarHorarios:string='http://localhost:8000/api/v1/car_wash/avaliable_schedules';
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  retornarHorarios(){
+    return this.http.get(this.urlRetornarHorarios);
+  }
 }
